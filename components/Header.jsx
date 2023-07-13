9e3f53eff93baf08e188e8b6b799c4b10bac0dbb
zAk <rashaandesnoyers@gmail.com>
2023-07-13 15:14:19 +0200

    Components updated

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../styles/Header.module.css';
 import Router from 'next/router';
+import Head from 'next/head';
 
 function Header() {
 
@@ -17,16 +18,26 @@ function Header() {
       </div>)
   });
   return (
-    <div className={styles.body}>
-        <h1
-          className={styles.title}
-          onClick={() => Router.push('/')}
-        >BURGER</h1>
-        {/* <div>Accueil Menu Offres Contact Icon</div> */}
-        <div className={styles.subHeader}>
-          {subHeader}
-        </div>
-    </div>
+    <>
+      <Head>
+        <title>Burger | Les meilleurs burgers de Paris</title>
+        <meta name="description" content="Les meilleurs burgers de Paris" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://res.cloudinary.com/dldeqai4u/image/upload/v1689252448/burger/burger_logo_ylhaaq.png" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
+      </Head>
+    
+      <div className={styles.body}>
+          <h1
+            className={styles.title}
+            onClick={() => Router.push('/')}
+          >BURGER</h1>
+          {/* <div>Accueil Menu Offres Contact Icon</div> */}
+          <div className={styles.subHeader}>
+            {subHeader}
+          </div>
+      </div>
+    </>
   )
 }
 
--- components/Home.jsx
+++ components/Home.jsx
@@ -5,77 +5,89 @@ import Header from './Header';
 import Header2 from './Header2';
 import Footer from './Footer';
 import Router from 'next/router';
+import Head from 'next/head';
+
 
 function Home() {
-  const [changeHeader, setChangeHeader] = useState(false);
 
   return (
-    <div className={styles.body} onScroll={() => setChangeHeader(!changeHeader)} >
-      {changeHeader ? <Header2 /> : <Header />}
+    <>
+      <Head>
+        <title>Burger | Les meilleurs burgers de Paris</title>
+        <meta name="description" content="Les meilleurs burgers de Paris" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://res.cloudinary.com/dldeqai4u/image/upload/v1689252448/burger/burger_logo_ylhaaq.png" />
+        <link href="https://vjs.zencdn.net/8.0.4/video-js.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"/>
+      </Head>
 
-      <div className={styles.container} onScroll={() => setChangeHeader(true)}>
-        <h1 className={styles.title}>
-          Burger, les meilleurs burgers de Paris.
-        </h1>
-      </div>
+      <div className={styles.body}>
+        <Header />
 
-      <div className={styles.about}>
-        <div>
-          <img src='burger6.png' width={370} style={{borderRadius: 20}} />
+        <div className={styles.container} onScroll={() => setChangeHeader(true)}>
+          <h1 className={styles.title}>
+            Burger, les meilleurs burgers de Paris.
+          </h1>
         </div>
-        <div className={styles.aboutRightPartContainer}>
-          <div className={styles.aboutHeader}>A PROPOS</div>
-          <div className={styles.aboutRightPart}>
-            <h2 className={styles.h2}>Title</h2>
-            <p className={styles.text}>Text</p>
-            <p className={styles.subText}>Subtext</p>
-            <button
-              className={styles.seeMoreBtn}
-              onClick={() => Router.push('/about')}>VOIR PLUS</button>
-          </div>
-        </div>
-      </div>
 
-      <div className={styles.specialOffersContainer}>
-        <div className={styles.specialOffers}>
+        <div className={styles.about}>
           <div>
-            <img src='brgr7.jpeg' width={450} style={{borderRadius: 15}} />
+            <img src='burger6.png' width={370} style={{borderRadius: 20}} />
           </div>
-
-          <div className={styles.specialOffersRightPartContainer}>
-            <div className={styles.specialOffersHeader}>PROMO</div>
-            <div className={styles.specialOffersRightPart}>
-              <h2 className={styles.h2}>TERIYAKI BURGER</h2>
-              <p className={styles.text}>Bun sésame (ou sans gluten +1€), artisanal steak haché de bœuf maturé (ou beyond meat +2€), cébette, sauce teriyaki maison, chou mariné, shichimi togarashi (mélange 7 épices), mayo japonaise.</p>
-              <p className={styles.price}>10,00€</p>
+          <div className={styles.aboutRightPartContainer}>
+            <div className={styles.aboutHeader}>A PROPOS</div>
+            <div className={styles.aboutRightPart}>
+              <h2 className={styles.h2}>Title</h2>
+              <p className={styles.text}>Text</p>
+              <p className={styles.subText}>Subtext</p>
               <button
-                className={styles.seeMenuBtn}
-                onClick={() => Router.push('/menu')}>VOIR LE MENU</button>
+                className={styles.seeMoreBtn}
+                onClick={() => Router.push('/about')}>VOIR PLUS</button>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.specialOffersContainer}>
+          <div className={styles.specialOffers}>
+            <div>
+              <img src='brgr7.jpeg' width={450} style={{borderRadius: 15}} />
+            </div>
+
+            <div className={styles.specialOffersRightPartContainer}>
+              <div className={styles.specialOffersHeader}>PROMO</div>
+              <div className={styles.specialOffersRightPart}>
+                <h2 className={styles.h2}>TERIYAKI BURGER</h2>
+                <p className={styles.text}>Bun sésame (ou sans gluten +1€), artisanal steak haché de bœuf maturé (ou beyond meat +2€), cébette, sauce teriyaki maison, chou mariné, shichimi togarashi (mélange 7 épices), mayo japonaise.</p>
+                <p className={styles.price}>10,00€</p>
+                <button
+                  className={styles.seeMenuBtn}
+                  onClick={() => Router.push('/menu')}>VOIR LE MENU</button>
+              </div>
+            </div>
+          </div>
+          
+          <div className={styles.specialOffers}>
+            <div>
+              <img src='brgr12.jpg' width={450} style={{borderRadius: 15}} />
+            </div>
+
+            <div className={styles.specialOffersRightPartContainer}>
+              <div className={styles.specialOffersHeader}>PROMO</div>
+              <div className={styles.specialOffersRightPart}>
+                <h2 className={styles.h2}>LOU SALEYA</h2>
+                <p className={styles.text}>Ce burger est un hommage culinaire au pays niçois et à sa gastronomie.</p>
+                <p className={styles.price}>9,00€</p>
+                <button
+                  className={styles.seeMenuBtn}
+                  onClick={() => Router.push('/menu')}>VOIR LE MENU</button>
+              </div>
             </div>
           </div>
         </div>
         
-        <div className={styles.specialOffers}>
-          <div>
-            <img src='brgr12.jpg' width={450} style={{borderRadius: 15}} />
-          </div>
-
-          <div className={styles.specialOffersRightPartContainer}>
-            <div className={styles.specialOffersHeader}>PROMO</div>
-            <div className={styles.specialOffersRightPart}>
-              <h2 className={styles.h2}>LOU SALEYA</h2>
-              <p className={styles.text}>Ce burger est un hommage culinaire au pays niçois et à sa gastronomie.</p>
-              <p className={styles.price}>9,00€</p>
-              <button
-                className={styles.seeMenuBtn}
-                onClick={() => Router.push('/menu')}>VOIR LE MENU</button>
-            </div>
-          </div>
-        </div>
+        <Footer />
       </div>
-      
-      <Footer />
-    </div>
+    </>
   );
 }
 
