d1eeb36111bf90784f7be74b015f70ef3d8ad76a
zAk <rashaandesnoyers@gmail.com>
2023-07-24 13:19:13 +0200

    First attempt to fix font-family issue

--- components/Home.jsx
+++ components/Home.jsx
@@ -36,9 +36,6 @@ function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="https://res.cloudinary.com/dldeqai4u/image/upload/v1689252448/burger/burger_logo_ylhaaq.png" />
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-
-        {/* <link href="https://vjs.zencdn.net/8.0.4/video-js.css" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossOrigin="anonymous"/> */}
       </Head>
 
 
